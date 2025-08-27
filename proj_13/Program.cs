823da26ca1133899135b8c100c99fb03794aed36
Divyansh-Sarvaria <divyanshsarvaria@gmail.com>
2025-08-27 19:12:48 +0530

    prog_13

---
+++ proj_13/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace program
+{
+  class cunst
+  {
+    public cunst()
+    {
+      Console.WriteLine("runing cunstructor");
+    }
+  }
+  class prog_13
+  {
+    public static void Main(string[] arg)
+    {
+      cunst res = new cunst();
+    }
+  }
+}
