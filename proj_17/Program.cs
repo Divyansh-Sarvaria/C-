0e9b9b331c9aa6a34b469eb647ddfa981ac0c0ca
Divyansh-Sarvaria <divyanshsarvaria@gmail.com>
2025-09-27 01:23:14 +0530

    prog_17

---
+++ proj_17/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+namespace prog{
+  public static class Kata
+  {
+    public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+    {
+      T previous = default(T);
+      bool first = true;
+
+      foreach (T item in iterable)
+      {
+        if (first || !item.Equals(previous))
+        {
+          yield return item;
+        }
+        previous = item;
+        first = false;
+      }
+    }
+  }
+}
